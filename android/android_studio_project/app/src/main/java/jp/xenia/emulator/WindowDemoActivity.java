015e945e8923da037c7ef9b149e9ee1d252557ba
asterisk <214192747+AsteriskTill@users.noreply.github.com>
2026-07-16 10:53:39 -0700

    fuck yeah

--- android/android_studio_project/app/src/main/java/jp/xenia/emulator/WindowDemoActivity.java
+++ android/android_studio_project/app/src/main/java/jp/xenia/emulator/WindowDemoActivity.java
@@ -1,18 +1,48 @@
 package jp.xenia.emulator;
 
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class WindowDemoActivity extends WindowedAppActivity {
-    @Override
-    protected String getWindowedAppIdentifier() {
-        return "xenia_ui_window_vulkan_demo";
+public class WindowDemoActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+    private boolean surfaceReady = false;
+    private String gamePath = null;
+
+    static {
+        System.loadLibrary("xenia-jni");
     }
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_window_demo);
-        setWindowSurfaceView(findViewById(R.id.window_demo_surface_view));
+
+        gamePath = getIntent().getStringExtra("game_path");
+
+        SurfaceView surfaceView = findViewById(R.id.surface);
+        surfaceView.getHolder().addCallback(this);
     }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        surfaceReady = true;
+        if (gamePath != null) {
+            nativeBootGame(gamePath, holder.getSurface());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        surfaceReady = false;
+        nativeShutdown();
+    }
+
+    private native void nativeBootGame(String gamePath, android.view.Surface surface);
+    private native void nativeShutdown();
+    private native boolean nativeIsRunning();
 }
