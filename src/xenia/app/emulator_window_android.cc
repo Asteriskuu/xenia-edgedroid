e674ff18fe122e574cc8f24510942ac3e078bfb4
asterisk <214192747+Asteriskuu@users.noreply.github.com>
2026-08-04 12:48:17 -0700

    what

--- src/xenia/app/emulator_window_android.cc
+++ src/xenia/app/emulator_window_android.cc
@@ -46,7 +46,7 @@ void EmulatorWindow::SetupGraphicsSystemPresenterPainting() {
     window->SetPresenter(presenter);
   }
 
-  if (emulator_->graphics_system() && 
+  if (emulator_->graphics_system() &&
       emulator_->graphics_system()->provider()) {
     immediate_drawer_ =
         emulator_->graphics_system()->provider()->CreateImmediateDrawer();
