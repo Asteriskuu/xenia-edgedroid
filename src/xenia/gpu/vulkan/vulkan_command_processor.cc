659012897d724ddc5e4c7c85c3dd100ea37a639d
Herman S. <429230+has207@users.noreply.github.com>
2026-08-07 22:14:41 +0900

    [Metal/Vulkan/Profiling] Instrument caches and draw paths

--- src/xenia/gpu/vulkan/vulkan_command_processor.cc
+++ src/xenia/gpu/vulkan/vulkan_command_processor.cc
@@ -2760,6 +2760,7 @@ void VulkanCommandProcessor::SubmitBarriersAndEnterRenderTargetCacheRenderPass(
 }
 
 void VulkanCommandProcessor::EndRenderPass() {
+  SCOPE_profile_cpu_f("gpu");
   assert_true(submission_open_);
   if (!in_render_pass_) {
     return;
